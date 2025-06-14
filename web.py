7d7f32025a824a47de5782aa78fd3c8cd218d60e
bot6798098-075634 <officialswatrp@gmail.com>
2025-06-14 10:52:22 +0100

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from threading import Thread
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Bot is running!"
 
-if __name__ == "__main__":
+def run_web():
     app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    Thread(target=run_web, daemon=True).start()
