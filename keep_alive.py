4562551609694f73b07d5575ee20ef4813cd9a28
bot6798098-075634 <officialswatrp@gmail.com>
2025-06-28 10:12:02 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "SWAT Roleplay Bot is alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    thread = Thread(target=run)
-    thread.start()
+    t = Thread(target=run)
+    t.start()
