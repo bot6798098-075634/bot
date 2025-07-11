4b69422be4a114605770f6064fd28cf6bbc841d0
bot6798098-075634 <officialswatrp@gmail.com>
2025-07-11 21:11:45 +0100

    Update slowmode.py
    Signed-off-by: bot6798098-075634 <officialswatrp@gmail.com>

--- cogs/slowmode.py
+++ cogs/slowmode.py
@@ -1,25 +1,38 @@
-# ------------------------ Slowmode slash command ------------------------
+import discord
+from discord import app_commands
+from discord.ext import commands
 
-@tree.command(name="slowmode", description="Set the slowmode duration for a channel")
-@app_commands.describe(seconds="Duration of slowmode in seconds")
-async def slowmode_slash(interaction: discord.Interaction, seconds: int):
-    staff_role = interaction.guild.get_role(staff_role_id)
-    if staff_role not in interaction.user.roles:
-        await interaction.response.send_message(
-            f"{failed_emoji} You don't have permission to use this command.",
-            ephemeral=True
+class Slowmode(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="slowmode", description="Set the slowmode duration for a channel")
+    @app_commands.describe(seconds="Duration of slowmode in seconds")
+    async def slowmode_slash(self, interaction: discord.Interaction, seconds: int):
+
+        staff_role = interaction.guild.get_role(staff_role_id)
+        if staff_role not in interaction.user.roles:
+            await interaction.response.send_message(
+                f"{failed_emoji} You don't have permission to use this command.",
+                ephemeral=True
+            )
+            return
+
+        await interaction.channel.edit(slowmode_delay=seconds)
+
+        embed = discord.Embed(
+            description=f"{time_emoji} Slowmode has been set to `{seconds}` seconds.",
+            color=discord.Color.green()
         )
-        return
 
-    await interaction.channel.edit(slowmode_delay=seconds)
+        if interaction.guild and interaction.guild.icon:
+            embed.set_thumbnail(url=interaction.guild.icon.url)
 
-    embed = discord.Embed(
-        description=f"{time_emoji} Slowmode has been set to `{seconds}` seconds.",
-        color=discord.Color.green()
-    )
+        embed.set_footer(text="SWAT Roleplay Community")
+        await interaction.response.send_message(embed=embed)
 
-    if interaction.guild and interaction.guild.icon:
-        embed.set_thumbnail(url=interaction.guild.icon.url)
-    embed.set_footer(text="SWAT Roleplay Community")
+    async def cog_load(self):
+        self.bot.tree.add_command(self.slowmode_slash)
 
-    await interaction.response.send_message(embed=embed)
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Slowmode(bot))
